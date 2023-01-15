dc875f144abb07c76513b817f6200f578230d5d5
cha <chyb627@naver.com>
2023-01-15 21:06:25 +0900

    geolocation 으로 현재 내 위치 가져오기

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import Geolocation from '@react-native-community/geolocation';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { Header } from '../components/Header/Header';
 
 export const MainScreen: React.FC = () => {
   // latitude: 37.4922459
   // longitude: 127.0881366
+
+  const [currentRegion, setCurrentRegion] = useState<{
+    latitude: number;
+    longitude: number;
+  }>({
+    latitude: 37.4922459,
+    longitude: 127.0881366,
+  });
+
+  console.log('currentRegion:::', currentRegion);
+
+  const getMyLocation = useCallback(() => {
+    Geolocation.getCurrentPosition((position) => {
+      setCurrentRegion({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    getMyLocation();
+  }, [getMyLocation]);
 
   return (
     <View style={styles.container}>
@@ -17,14 +41,20 @@ export const MainScreen: React.FC = () => {
         style={styles.map}
         region={{
           // 위치
-          latitude: 37.4922459,
-          longitude: 127.0881366,
+          latitude: currentRegion.latitude,
+          longitude: currentRegion.longitude,
 
           // 지도가 보이는 화면의 크기
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
-        }}
-      />
+        }}>
+        <Marker
+          coordinate={{
+            latitude: currentRegion.latitude,
+            longitude: currentRegion.longitude,
+          }}
+        />
+      </MapView>
     </View>
   );
 };
