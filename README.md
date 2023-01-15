391cb66d235ef0b5d0f474d874c83ddb9b0a808a
cha <chyb627@naver.com>
2023-01-15 20:42:33 +0900

    react-native-maps로 google 지도 띄우기

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mustgorestaurant
+
 React-Native 맛집 공유 앱
+
+## maps
+
+maps.google.com에서 좌표를 알 수 있다.
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 import { Header } from '../components/Header/Header';
 
 export const MainScreen: React.FC = () => {
+  // latitude: 37.4922459
+  // longitude: 127.0881366
+
   return (
-    <View>
+    <View style={styles.container}>
       <Header>
         <Header.Title title="Main" />
       </Header>
+
+      <MapView
+        style={styles.map}
+        region={{
+          // 위치
+          latitude: 37.4922459,
+          longitude: 127.0881366,
+
+          // 지도가 보이는 화면의 크기
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
